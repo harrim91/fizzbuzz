e672d556b2ce74cba4ba0a07403304977aec7d3b
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-06 15:41:28 +0100

    passed when number is divisible by 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz num
-  return "fizzbuzz" if num == 15
+  return "fizzbuzz" if num % 15 == 0
   return "buzz" if num % 5 == 0
   return "fizz" if num % 3 == 0
   num
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,8 +13,10 @@ describe 'fizzbuzz' do
     end
   end
 
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+    (1..100).each do |n|
+      expect(fizzbuzz(n*15)).to eq 'fizzbuzz'
+    end
   end
 
   it 'returns the number when not divisible by 3 or 5' do
