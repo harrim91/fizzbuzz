a7038128ce04883b9bbbbfe5c32512ae75c4d446
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-06 15:24:05 +0100

    passed 4th rspec test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,6 @@
 def fizzbuzz num
   return "fizzbuzz" if num == 15
   return "buzz" if num == 5
+  return num if num % 3 != 0 && num % 5 != 0
   "fizz"
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,7 +10,11 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
-  it 'returns "fizzbuzz" when passed 15'do
+  it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns the number when not divisible by 3 or 5' do
+    expect(fizzbuzz(2)).to eq 2
   end
 end
