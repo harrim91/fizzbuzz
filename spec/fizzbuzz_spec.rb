2c5ff2d8376b452f363dd00fb69e1a7c5879f4a0
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-06 19:57:54 +0100

    fixed typos in test descriptions and added better bignum tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,37 +2,34 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  # it 'returns "fizz" when passed a multiple of 3' do
-  #   (1..100).each do |n|
-  #     expect(fizzbuzz(n*3)).to eq 'fizz' if n*3 % 5 != 0
-  #   end
-  # end
-
   it 'returns "fizz" when called on a multiple of 3' do
     (1..100).each do |n|
       expect((n*3).fizzbuzz).to eq 'fizz' if n*3 % 5 != 0
     end
   end
 
-  it 'works for bignums' do
-    expect(4611686018427387904.fizzbuzz).to eq 4611686018427387904
-  end
-
-  it 'returns "buzz" when passed a multiple of 5' do
+  it 'returns "buzz" when called on a multiple of 5' do
     (1..100).each do |n|
       expect((n*5).fizzbuzz).to eq 'buzz' if n*5 % 3 != 0
     end
   end
 
-  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+  it 'returns "fizzbuzz" when called on a multiple of 15' do
     (1..100).each do |n|
       expect((n*15).fizzbuzz).to eq 'fizzbuzz'
     end
   end
 
-  it 'returns the number when not divisible by 3 or 5' do
+  it 'returns self when not divisible by 3 or 5' do
     (1..100).each do |n|
       expect(n.fizzbuzz).to eq n if n % 3 != 0 && n % 5 != 0
     end
+  end
+
+  it 'works on bignums' do
+    expect(4611686018427387904.fizzbuzz).to eq 4611686018427387904
+    expect(4611686018427387905.fizzbuzz).to eq "buzz"
+    expect(4611686018427387906.fizzbuzz).to eq "fizz"
+    expect(4611686018427387915.fizzbuzz).to eq "fizzbuzz"
   end
 end
